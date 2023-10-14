c5ae094b67ff60dd41b7928b43034fad9cf3cd13
Anirudh Jwala <anirudhsai2@gmail.com>
2023-10-14 17:15:06 +0530

    Loading animations added for cover image page

--- components/cover.tsx
+++ components/cover.tsx
@@ -13,6 +13,7 @@ import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface CoverImageProps {
   url?: string;
@@ -69,4 +70,8 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     </div>
   );
+};
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="w-full h-[12vh]" />;
 };
