82c92ab6102785f0891bf1ffdcaba9b5d6bad001
Anirudh Jwala <anirudhsai2@gmail.com>
2023-10-14 15:59:25 +0530

    Icon picker for the document

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,38 @@
-const DocumentIdPage = () => {
-  return <div>document id</div>;
+'use client';
+
+import { useQuery } from 'convex/react';
+
+import { Id } from '@/convex/_generated/dataModel';
+import { api } from '@/convex/_generated/api';
+
+import { Toolbar } from '@/components/toolbar';
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<'documents'>;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-4xl mx-auto"></div>
+      <Toolbar initialData={document} />
+    </div>
+  );
 };
 
 export default DocumentIdPage;
